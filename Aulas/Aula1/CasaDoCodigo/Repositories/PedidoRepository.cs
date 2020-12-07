9ad8a0e1bfd08dc2f2b0185dd7a0e7c45a496dd5
talesboalim <talesboalim@gmail.com>
2020-12-07 00:08:13 -0300

    Projeto finalizado. Produto é incluído no carrinho e iniciado Finalizar Pedido

--- Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,5 +1,6 @@
 ﻿using CasaDoCodigo.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,11 @@ using System.Threading.Tasks;
 
 namespace CasaDoCodigo.Repositories
 {
-    public interface IPedidoRepository { }
+    public interface IPedidoRepository
+    {
+        Pedido GetPedido();
+        void AddItem(string codigo);
+    }
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
     {
         private readonly IHttpContextAccessor contextAccessor;
@@ -15,6 +20,54 @@ namespace CasaDoCodigo.Repositories
                 IHttpContextAccessor contextAccessor) : base(contexto)
         {
             this.contextAccessor = contextAccessor;
+        }
+
+        public void AddItem(string codigo)
+        {
+            var produto = contexto.Set<Produto>()
+                        .Where(p => p.Codigo == codigo)
+                        .SingleOrDefault();
+
+            if (produto == null)
+            {
+                throw new ArgumentException("Produto não encontrado");
+            }
+
+            var pedido = GetPedido();
+
+            var itemPedido = contexto.Set<ItemPedido>()
+                                .Where(i => i.Produto.Codigo == codigo
+                                    && i.Pedido.Id == pedido.Id)
+                                .SingleOrDefault();
+
+            if (itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                contexto.Set<ItemPedido>()
+                                .Add(itemPedido);
+
+                contexto.SaveChanges();
+            }
+        }
+
+        public Pedido GetPedido()
+        {
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet
+                .Include(p => p.Itens)
+                    .ThenInclude(i => i.Produto)
+                .Where(p => p.Id == pedidoId)
+                .SingleOrDefault();
+
+            if (pedido == null)
+            {
+                pedido = new Pedido();
+                dbSet.Add(pedido);
+                contexto.SaveChanges();
+                SetPedidoId(pedido.Id);
+            }
+
+            return pedido;
         }
 
         private int? GetPedidoId()
---
+++ README.md
@@ -0,0 +1,9 @@
+# ASP.NET CORE PARTE 1: UM E-COMMERCE COM MVC E EF CORE
+
+## Requisitos
+-Microsoft SQL Server 2019
+-ASP.NET Core 2.0
+
+## Execução
+-Criar BD no SQL Server como CasaDoCodigo
+-Alterar a string de conexão no arquivo appsettings.json em ConnectionStrings
